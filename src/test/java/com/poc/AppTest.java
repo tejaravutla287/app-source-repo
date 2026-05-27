deacb2166dad38115e6d44482b3672a487c39727
tejaravutla287 <tejaravutla287@gmail.com>
2026-05-27 10:47:39 +0530

    Create AppTest.java

---
+++ src/test/java/com/poc/AppTest.java
@@ -0,0 +1,7 @@
+package com.poc;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest(classes = App.class)
+class AppTest {
+    @Test void contextLoads() {}
+}
